0583960ff81620c51415440ab60362b74e0c636d
ShivamSaini490 <126521640+ShivamSaini490@users.noreply.github.com>
2023-12-04 14:47:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        try
+        {
+            int index = 10; 
+            int value = numbers[index];
+            Console.WriteLine("Value at index " + index + ": " + value);
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            
+        }
+
+        Console.WriteLine("Program continues...");
+    }
+}
